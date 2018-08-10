d22b57a5f0d49ca23928a03e814b62bee2175b6f
Rahul Patil <rp7187655@gmail.com>
2018-08-10 19:15:49 +0530

    add greatestOf function

--- src/basic.js
+++ src/basic.js
@@ -65,6 +65,16 @@ const cube = function(number){
   greatestOf
   This function returns the greatest of three numbers
 */
+const greater = function(num1,num2){
+  if(num1>=num2) return num1;
+  return num2;
+}
+
+
+const greatestOf = function(num1,num2,num3){
+  let greaterOfTwo = greater(num1,num2);
+  return greater(greaterOfTwo,num3);
+}
 
 
 /*
